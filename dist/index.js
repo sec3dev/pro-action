608d3792d38f40b52a067bb0e5e08a6af759b840
Jihang Li <lijihang26@gmail.com>
2022-04-13 20:31:19 -0400

    Improve the output

--- dist/index.js
+++ dist/index.js
@@ -16340,20 +16340,21 @@ const saveFilename = 'soteria-report.sarif';
 async function run() {
     try {
       const password = core.getInput('soteria-token', {required: true});
-      const issue = github.context.issue;
       execSync(`base=$(basename $PWD)
                 cd ..
                 tar -czf /tmp/code.tgz $base`);
       const formData = new FormData();
-      const taskName = github.context.payload.repository.name + ' ' + (new Date()).toLocaleString();
+      const commit = github.context.sha;
+      const taskName = github.context.payload.repository.name + ' ' + commit;
       formData.append('taskName', taskName);
       formData.append('description', '');
       formData.append('password', password);
+      formData.append('commit', commit);
       formData.append('code', fs.createReadStream('/tmp/code.tgz'), 'name');
       const formHeaders = formData.getHeaders();
 
       core.info('Analyzing code...');
-      const response = await axios.post(`${serverUrl}/${apiVersion}/action`, formData, {timeout: 600000}, {
+      const response = await axios.post(`${serverUrl}/${apiVersion}/action`, formData, {
         headers: {...formHeaders}
       });
       if (response.data.report) {
@@ -16363,7 +16364,13 @@ async function run() {
             throw error;
           }
         });
+
         core.info('Analysis completed!');
+        if (response.data.numTotalIssues === 0) {
+          core.info(`All tests are passed!`);
+        } else {
+          core.setFailed(`Total number of warnings: ${response.data.numTotalIssues}`)
+        }
         core.info(`The report is saved in the workspace as "${saveFilename}"`);
         core.info(`To view and download the report on Soteria web app, visit: ${response.data.reportLink}`);
       } else {
